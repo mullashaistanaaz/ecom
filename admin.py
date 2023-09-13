2d46ece36c86ae3da6321b3f347ede7d1830d225
zakiya-kudchi <127715325+zakiya-kudchi@users.noreply.github.com>
2023-09-13 12:29:21 +0530

    Add files via upload

---
+++ admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Customer,Product,Category,Catelog,Order,ShippingAddress,Content,AdminProfile
+from .models import Orderitem,SearchQuery,CustomerSupport,SalesData
+from .models import *
+
+# Register your models here.
+
+admin.site.register(SalesData)
+admin.site.register(AdminProfile)
+admin.site.register(Content)
+admin.site.register(CustomerSupport)
+admin.site.register(ShippingAddress)
+admin.site.register(Order)
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Catelog)
+admin.site.register(Orderitem)
+# admin.site.register(Rating)
+admin.site.register(SearchQuery)
+# admin.site.register(UserPurchaseHistory)
--- meta/settings.py
+++ meta/settings.py
@@ -132,3 +132,7 @@ MEDIA_ROOT = BASE_DIR / "app/static/images"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
